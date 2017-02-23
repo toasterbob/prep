827e55c0a5c302b8d4d53842d56f28c5b60ddca0
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-22 21:20:16 -0800

    w1d4

--- w1/w1d4/exercises/lib/04_dictionary.rb
+++ w1/w1d4/exercises/lib/04_dictionary.rb
@@ -1,3 +1,34 @@
 class Dictionary
-  # TODO: your code goes here!
+  attr_accessor :entries
+
+  def initialize
+    @entries = {}
+  end
+
+  def add(entry)
+    if entry.is_a?(String)
+      @entries[entry] = nil
+    elsif entry.is_a?(Hash)
+      @entries.merge!(entry)
+    end
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include?(word)
+    self.keywords.include?(word)
+  end
+
+  def find(word)
+    @entries.select { |key, definition| key.match(word)}
+  end
+
+  def printable
+    @entries.each do |key, definition|
+      
+    end
+  end
+
 end
