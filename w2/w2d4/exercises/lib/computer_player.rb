6f073360ca0dff08796757cd62177c034a7b87a0
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-24 19:39:40 -0800

    tic tac toe

--- w2/w2d4/exercises/lib/computer_player.rb
+++ w2/w2d4/exercises/lib/computer_player.rb
@@ -1,2 +1,14 @@
 class ComputerPlayer
+
+  attr_reader :name, :mark
+
+  def initialize(name = "computer", mark)
+    @name = name
+    @mark = mark
+  end
+
+  def display
+
+  end
+  
 end
--- w2/w2d4/exercises/lib/human_player.rb
+++ w2/w2d4/exercises/lib/human_player.rb
@@ -1,2 +1,14 @@
 class HumanPlayer
+
+  attr_reader :name, :mark
+
+  def initialize(name, mark)
+    @name = name
+    @mark = mark
+  end
+
+  def display
+    
+  end
+
 end
