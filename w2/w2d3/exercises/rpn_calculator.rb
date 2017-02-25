356ec778128017e66835d5537282b3dc6309a075
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-24 19:32:47 -0800

    tic tac toe

---
+++ w2/w2d3/exercises/rpn_calculator.rb
@@ -0,0 +1,6 @@
+require_relative "../w2d1/lib/00_rpn_calculator.rb"
+
+class RPNCalculator
+
+
+end
--- w2/w2d4/exercises/lib/board.rb
+++ w2/w2d4/exercises/lib/board.rb
@@ -1,2 +1,36 @@
 class Board
+  attr_reader :grid
+
+  def initialize(grid = Array.new(3) {Array.new(3)})
+    @grid = grid
+  end
+
+  def place_mark(pos, mark)
+    # raise "Spot already taken" unless self.empty?(pos)
+    x, y = pos
+    self.grid[x][y] = mark
+  end
+
+  def empty?(pos)
+    x, y = pos
+    @grid[x][y].nil?
+  end
+
+  def winner
+    (0..2).each do |i|
+      return @grid[i][0] if @grid[i][0] && [@grid[i][0], @grid[i][1], @grid[i][2]].uniq.length == 1
+      return @grid[0][i] if @grid[0][i] && [@grid[0][i], @grid[1][i], @grid[2][i]].uniq.length == 1
+    end
+    return @grid[0][0] if @grid[0][0] && [@grid[0][0], @grid[1][1], @grid[2][2]].uniq.length == 1
+    return @grid[0][2] if @grid[0][2] && [@grid[0][2], @grid[1][1], @grid[2][0]].uniq.length == 1
+    nil
+  end
+
+  def over?
+    @grid.flatten.none? { |pos| pos.nil? } || winner
+  end
+
+
+
+
 end
