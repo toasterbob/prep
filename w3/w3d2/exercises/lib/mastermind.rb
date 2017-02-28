4168e73ca7a6b1b14fe4fe8e97577da580737679
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-27 18:12:21 -0800

    mastermind

--- w3/w3d2/exercises/lib/mastermind.rb
+++ w3/w3d2/exercises/lib/mastermind.rb
@@ -1,5 +1,26 @@
 class Code
+  PEGS = {"R" => "Red", "G" => "Green", "B" => "Blue", "Y" => "Yellow",
+          "O" => "Orange", "P" => "Purple"}
+
   attr_reader :pegs
+
+  def initialize(pegs)
+    @pegs = pegs
+  end
+
+  def parse
+
+  end
+
+  def self.random
+    colors = %w(R G B Y O P)
+    results = []
+    4.times do
+      results << colors.sample
+    end
+    Code.new(results)
+  end
+
 end
 
 class Game
