7bfaa61abfa5b91619b0ec137a674880b8ffb4ab
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-27 22:25:00 -0800

    battleship

--- w3/w3d3/exercises/lib/board.rb
+++ w3/w3d3/exercises/lib/board.rb
@@ -1,2 +1,14 @@
 class Board
+
+  attr_reader :grid
+
+  def self.default_grid
+    Array.new(10) { Array.new(10) }
+  end
+
+  def initialize(grid = self.class.default_grid)
+    @grid = grid
+  end
+
+
 end
