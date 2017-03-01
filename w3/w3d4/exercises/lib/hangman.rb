bd8002cca4a257ea962acd042547cb61709a3f22
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-28 22:04:24 -0800

    hangman phase1

--- w3/w3d4/exercises/lib/hangman.rb
+++ w3/w3d4/exercises/lib/hangman.rb
@@ -1,9 +1,69 @@
 class Hangman
   attr_reader :guesser, :referee, :board
+
+  def initialize(guessing_player, checking_player)
+    @guesser = guessing_player
+    @referee = checking_player
+  end
+
+
 end
 
 class HumanPlayer
+  def initialize(name = "Jimbob")
+    @name = name
+  end
+
+  def pick_secret_word
+
+  end
+
+  def register_secret_length
+
+  end
+
+  def guess
+
+  end
+
+  def check_guess
+
+  end
+
+  def handle_response
+
+  end
 end
 
 class ComputerPlayer
+  attr_reader :secret_word
+
+  def initialize(dictionary)
+    @dictionary = dictionary
+  end
+
+  def pick_secret_word
+    @secret_word = @dictionary.sample
+    secret_word.length
+  end
+
+  def register_secret_length
+
+  end
+
+  def guess
+
+  end
+
+  def check_guess(letter)
+    indices = []
+    secret_word.each_char.with_index do |char, i|
+      indices << i if char == letter
+    end
+    indices
+  end
+
+  def handle_response
+
+  end
 end
