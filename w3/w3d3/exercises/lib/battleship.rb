8c78fa283378a38cdbde0aac17878d29626ae1eb
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-28 19:46:57 -0800

    board battleship

--- w3/w3d3/exercises/lib/battleship.rb
+++ w3/w3d3/exercises/lib/battleship.rb
@@ -1,11 +1,29 @@
+require_relative "board"
+require_relative "player"
+
 class BattleshipGame
 
   attr_reader :board, :player
 
-  def initialize(board = Board.new, player = Player.new)
+  def initialize(player = HumanPlayer.new("Mark"), board = Board.new)
     @board = board
     @player = player
+    @hit = false
   end
 
-  
+  def attack(pos)
+    if board[pos] == :s
+      @hit = true
+    else
+      @hit = false
+    end
+
+    board[pos] = :x
+  end
+
+  def count
+    board.count 
+  end
+
+
 end
